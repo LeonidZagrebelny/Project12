7f6d5b919e5f9472ee44cec01c141ac96b3bd22d
Leonid <navymoves63723@gmail.com>
2025-08-19 18:43:44 +0500

    Third commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public float calculate(float height, float weight) {
-        float massIndex = (int)(weight / (height * height));
-        return massIndex;
+        float massIndex = (weight / (height * height));
+        return (int)massIndex;
 
     }
 }
